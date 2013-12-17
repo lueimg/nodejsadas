f7533b1f8de5be1a32f669a8bafa9239739898e4
luis mori <luismorith@gmail.com>
2013-12-17 01:27:22 -0500

    configurar socket.io

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require("express"),
+var express = require("express.io"),
 	swig = require("swig"),
 	_ = require("underscore");
+
 var server = express();
+server.http().io();
 
 var RedisStore = require("connect-redis")(express);
 
@@ -9,6 +11,9 @@ var RedisStore = require("connect-redis")(express);
 server.engine("html",swig.renderFile);
 server.set("view engine","html");
 server.set("views","./app/views");
+
+server.use(express.static("./public"));
+
 
 server.configure(function(){
 	server.use(express.logger());
