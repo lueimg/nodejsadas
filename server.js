08d148c6bcf0bd160e539e7ff9e640f048581607
luis mori <luismorith@gmail.com>
2013-12-17 21:06:37 -0500

    craecion del controllador home

--- server.js
+++ server.js
@@ -33,8 +33,6 @@ server.configure(function(){
 var homeController = require("./app/controllers/home");
 homeController(server);
 
-
-
 var users = [];
 
 var isntLoggedIn = function(req,res,next){
@@ -46,47 +44,12 @@ var isntLoggedIn = function(req,res,next){
 	next();
 };
 
-var isLoggedIn = function(req,res,next){
-
-	if(req.session.user){
-		res.redirect("/app");
-		return;
-	}
-	next();
-};
-
-server.get("/", isLoggedIn, function(req,res){
-	//res.send("hola");
-	res.render("home");
-});
-
-
-server.post("/log-in",function(req,res){
-	users.push(req.body.username);
-
-	server.io.broadcast("log-in",{user:req.body.username});
-
-
-	req.session.user = req.body.username;
-	res.redirect("/app");
-});
 
 server.get("/app", isntLoggedIn, function(req,res){
 	res.render("app",{
 		user:req.session.user,
 		users:users
 	});
-
-});
-
-
-server.get("/log-out",function(req,res){
-	users  = _.without(users , req.session.user );
-	
-	server.io.broadcast("log-out",{user:req.session.user });
-
-	req.session.destroy();
-	res.redirect("/");
 
 });
 
